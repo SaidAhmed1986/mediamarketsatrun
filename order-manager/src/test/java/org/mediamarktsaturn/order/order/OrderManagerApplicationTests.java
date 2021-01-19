4fb6c8b08d22f85d7dcebde1fec8ad311e6ad558
SaidAhmed1986 <46595071+SaidAhmed1986@users.noreply.github.com>
2021-01-19 21:53:04 +0100

    initial commit

---
+++ order-manager/src/test/java/org/mediamarktsaturn/order/order/OrderManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mediamarktsaturn.order.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
